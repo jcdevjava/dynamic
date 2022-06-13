583fcc4fded958c6e2369f7d6c8bd7f0cfbcbc30
José Carlos Palma <palmahn@gmail.com>
2022-06-13 16:55:46 -0600

    init(app): Creación del proyecto.

---
+++ src/main/java/xyz/jcpalma/dynamic/Application.java
@@ -0,0 +1,9 @@
+package xyz.jcpalma.dynamic;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplos de Programación Dinámica");
+    }
+
+}
